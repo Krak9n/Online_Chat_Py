91741e0388055e30a8958a5fcd1cbc9c9e99f57e
Krak9n <alyoshagnatu@gmail.com>
2025-07-28 18:23:53 +0200

    port update

--- main.py
+++ main.py
@@ -243,4 +243,4 @@ def disconnect():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, host='0.0.0.0', port=80)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
